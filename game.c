f7be19506f302775344a96231edd60b356a3c7a8
Aapo Vienamo <aapo.vienamo@iki.fi>
2017-07-19 00:24:09 +0300

    game: Add some vec3 math functions

--- game.c
+++ game.c
@@ -15,6 +15,24 @@ struct vec3 {
     };
 };
 
+static inline struct vec3 vec3_add(const struct vec3 a, const struct vec3 b)
+{
+    struct vec3 s = { a.x + b.x, a.y + b.y, a.z + b.z };
+    return s;
+}
+
+static inline struct vec3 vec3_muls(const struct vec3 a, const float b)
+{
+    struct vec3 r = { a.x * b, a.y * b, a.z * b };
+    return r;
+}
+
+static inline struct vec3 vec3_pow2(const struct vec3 a)
+{
+    struct vec3 r = { a.x * a.x, a.y * a.y, a.z * a.z };
+    return r;
+}
+
 struct phys_comp  {
     struct vec3 pos;
     struct vec3 vel;
