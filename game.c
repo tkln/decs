2241231687dda9647eaaa6e50c871e79f0ddc052
Aapo Vienamo <aapo.vienamo@iki.fi>
2017-07-29 00:07:31 +0300

    game: Add gravity, multiple visual changes

--- game.c
+++ game.c
@@ -32,6 +32,7 @@ struct sys_ids {
     uint64_t phys;
     uint64_t phys_drag;
     uint64_t force_field;
+    uint64_t gravity;
     uint64_t render;
 };
 
@@ -78,14 +79,25 @@ static void phys_tick(struct decs *decs, uint64_t eid, void *func_data)
 
     phys->force = (struct vec3){ 0.0f, 0.0f, 0.0f };
 
-    /* Wraparound */
-    phys->pos.x = fmod(phys->pos.x, 1.0f);
-    phys->pos.y = fmod(phys->pos.y, 1.0f);
+    if (phys->pos.y > 1.0f) {
+        phys->pos.y -= 0.1f;
+        phys->vel.y *= -0.9f;
+    }
+    if (phys->pos.x > 1.0f)
+        phys->pos.x -= 1.0f;
     if (phys->pos.x < 0.0f)
         phys->pos.x += 1.0f;
-    if (phys->pos.y < 0.0f)
-        phys->pos.y += 1.0f;
 }
+
+static void gravity_tick(struct decs *decs, uint64_t eid, void *func_data)
+{
+    uint64_t *phys_comp_id = func_data;
+    struct phys_comp *phys = decs_get_comp(decs, *phys_comp_id, eid);
+
+    phys->force  = vec3_add(phys->force, (struct vec3) { 0.0f, 9.81f, 0.0f });
+}
+
+int win_w = 1280, win_h = 720;
 
 static void render_tick(struct decs *decs, uint64_t eid, void *func_data)
 {
@@ -93,19 +105,10 @@ static void render_tick(struct decs *decs, uint64_t eid, void *func_data)
     struct phys_comp *phys = decs_get_comp(decs, ctx->comp_ids->phys, eid);
     struct color_comp *color = decs_get_comp(decs, ctx->comp_ids->color, eid);
 
-#if 0
-    /* XXX */
-    printf("\n%s(%lu):\ncolor.r: %f, color.g: %f, color.b: %f\n", __func__, eid,
-           color->r, color->g, color->b);
-    printf("\n%s(%lu):\nphys.pos.x: %f, phys.pos.y: %f, phys.pos.z: %f,\n"
-           "phys.vel.x: %f, phys.vel.y: %f, phys.vel.z: %f\n", __func__, eid,
-           phys->pos.x, phys->pos.y, phys->pos.z,
-           phys->vel.x, phys->vel.y, phys->vel.z);
-#endif
 
     SDL_SetRenderDrawColor(ctx->rend, 0xff * color->r, 0xff * color->g,
                            0xff * color->b, 0xff);
-    SDL_RenderDrawPoint(ctx->rend, phys->pos.x * 640, phys->pos.y * 480);
+    SDL_RenderDrawPoint(ctx->rend, phys->pos.x * win_w, phys->pos.y * win_h);
 
 }
 
@@ -136,19 +139,18 @@ void create_particle(struct decs *decs, struct comp_ids *comp_ids)
     phys = decs_get_comp(decs, comp_ids->phys, eid);
     color = decs_get_comp(decs, comp_ids->color, eid);
 
-    /* XXX */
-    phys->pos.x = eid * 0.01f;
-    phys->pos.y = eid * 0.02f;
-    phys->pos.z = eid * 0.03f;
-    phys->vel.x = sin(eid * 0.1) * 0.004f;
-    phys->vel.y = sin(eid * 0.1) * 0.005f;
-    phys->vel.z = sin(eid * 0.1) * 0.006f;
+    phys->pos.x = 0.5f;
+    phys->pos.y = 0.25f;
+    phys->pos.z = 0.0f;
+    phys->vel.x = cos(eid * 0.25) * 0.2f;
+    phys->vel.y = sin(eid * 0.25) * 0.2f;
+    phys->vel.z = sin(eid * 0.25) * 0.2f;
     phys->force.x = 0.0f;
     phys->force.y = 0.0f;
     phys->force.z = 0.0f;
-    phys->mass = 1.0f;
-    color->r = eid * 0.1 * 2;
-    color->g = eid * 0.3 * 2;
+    phys->mass = 5.0f;
+    color->r = sin(eid * 0.1) * 2;
+    color->g = cos(eid * 0.3) * 2;
     color->b = eid * 0.2 * 2;
 }
 
@@ -172,7 +174,7 @@ int main(void)
 
     /* TODO error checks */
     win = SDL_CreateWindow("title", SDL_WINDOWPOS_UNDEFINED,
-                           SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
+                           SDL_WINDOWPOS_UNDEFINED, win_w, win_h, 0);
     rend = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
 
     render_ctx.comp_ids = &comp_ids;
@@ -188,22 +190,25 @@ int main(void)
 
     render_comps = (1<<comp_ids.phys) | (1<<comp_ids.color);
 
+#if 0
     sys_ids.force_field = decs_register_system(&decs, 1<<comp_ids.phys,
                                                force_field_tick,
                                                &force_field_ctx, NULL);
+#endif
+    sys_ids.gravity = decs_register_system(&decs, 1<<comp_ids.phys,
+                                           gravity_tick, &comp_ids.phys,
+                                           NULL);
     sys_ids.phys_drag = decs_register_system(&decs, 1<<comp_ids.phys,
                                              phys_drag_tick, &comp_ids.phys,
                                              NULL);
     sys_ids.phys = decs_register_system(&decs, 1<<comp_ids.phys, phys_tick,
                                         &comp_ids.phys,
-                                        SYS_IDS_ARR(sys_ids.force_field,
-                                                    sys_ids.phys_drag));
+                                        SYS_IDS_ARR(/*sys_ids.force_field,*/
+                                                    sys_ids.phys_drag,
+                                                    sys_ids.gravity));
     sys_ids.render = decs_register_system(&decs, render_comps, render_tick,
                                           &render_ctx,
                                           SYS_IDS_ARR(sys_ids.phys));
-
-    for (i = 0; i < 2048; ++i)
-        create_particle(&decs, &comp_ids);
 
     while (runnig) {
         while (SDL_PollEvent(&event)) {
@@ -214,10 +219,11 @@ int main(void)
         create_particle(&decs, &comp_ids);
 
         SDL_GetMouseState(&mx, &my);
-        force_field_ctx.center_point.x = mx / 640.0f;
-        force_field_ctx.center_point.y = my / 480.0f;
+        force_field_ctx.center_point.x = mx / win_w;
+        force_field_ctx.center_point.y = my / win_h;
 
-        SDL_SetRenderDrawColor(rend, 0x22, 0x22, 0x22, 0xff);
+        //SDL_SetRenderDrawColor(rend, 0x22, 0x22, 0x22, 0xff);
+        SDL_SetRenderDrawColor(rend, 0x0, 0x0, 0x0, 0xff);
         SDL_RenderClear(rend);
 
         decs_tick(&decs);
