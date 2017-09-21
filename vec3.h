6d27ca4436c1254ea3ea87435ba329f590873943
Aapo Vienamo <aapo.vienamo@iki.fi>
2017-09-21 16:08:03 +0300

    vec3: Reformat

--- vec3.h
+++ vec3.h
@@ -12,14 +12,12 @@ struct vec3 {
 
 static inline struct vec3 vec3_add(const struct vec3 a, const struct vec3 b)
 {
-    struct vec3 s = { a.x + b.x, a.y + b.y, a.z + b.z };
-    return s;
+    return (struct vec3) { a.x + b.x, a.y + b.y, a.z + b.z };
 }
 
 static inline struct vec3 vec3_sub(const struct vec3 a, const struct vec3 b)
 {
-    struct vec3 s = { a.x - b.x, a.y - b.y, a.z - b.z };
-    return s;
+    return (struct vec3) { a.x - b.x, a.y - b.y, a.z - b.z };
 }
 
 static inline float vec3_sum(const struct vec3 a)
@@ -34,22 +32,19 @@ static inline struct vec3 vec3_inv(const struct vec3 a)
 
 static inline struct vec3 vec3_muls(const struct vec3 a, const float b)
 {
-    struct vec3 r = { a.x * b, a.y * b, a.z * b };
-    return r;
+    return (struct vec3) { a.x * b, a.y * b, a.z * b };
 }
 
 static inline struct vec3 vec3_spow2(const struct vec3 a)
 {
-    struct vec3 r = { a.x * a.x * (a.x < 0.0f ? -1.0f : 1.0f),
-                      a.y * a.y * (a.y < 0.0f ? -1.0f : 1.0f),
-                      a.z * a.z * (a.z < 0.0f ? -1.0f : 1.0f) };
-    return r;
+    return (struct vec3) { a.x * a.x * (a.x < 0.0f ? -1.0f : 1.0f),
+                           a.y * a.y * (a.y < 0.0f ? -1.0f : 1.0f),
+                           a.z * a.z * (a.z < 0.0f ? -1.0f : 1.0f) };
 }
 
 static inline struct vec3 vec3_pow2(const struct vec3 a)
 {
-    struct vec3 r = { a.x * a.x, a.y * a.y, a.z * a.z };
-    return r;
+    return (struct vec3) { a.x * a.x, a.y * a.y, a.z * a.z };
 }
 
 #endif
